f365c69f340d0167f5a7aae862eceb2ec729f180
bah-abdourahmane <bahabdourahimane@gmail.com>
2023-07-02 20:22:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Products from "./pages/Products";
+import Layout from "./layout/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/products/:id", element: <Products /> },
+      { path: "/product/:id", element: <Product /> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
